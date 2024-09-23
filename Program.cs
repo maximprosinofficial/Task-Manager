22ad46b7e410cd1f5af1a5a591c94101fc9bafdb
maximprosinofficial <maximprosin647@gmail.com>
2024-09-23 21:17:11 +0300

    add design

--- Program.cs
+++ Program.cs
@@ -23,6 +23,8 @@ class TaskManager
 
     static void Main()
     {
+        Console.Title = "Task Manager | x1mhp.sys";
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine(@"
 ``_____`````````_``````__``__``````````````````````````` ```````` 
 `|_```_|_`_`___|`|`__`|``\/``|`__`_`_`__```__`_` `__`_``___`_`__` 
@@ -33,13 +35,17 @@ class TaskManager
 ");
         Console.WriteLine("Разработчик: x1mhp.sys");
         Console.WriteLine("Нажмите любую клавишу для продолжения");
+        
+        Console.ResetColor();
 
         Console.ReadKey();
 
         while (true)
         {
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("\nВыберите действие: \n1. Добавить задачу \n2. Просмотреть текущие задачи \n3. Отметить задачу как выполненную \n4. Удалить задачу \n5. Посмотреть выполненные задачи \n6. Посмотреть удаленные задачи \n7. Выйти\n");
-
+            Console.ResetColor();
+            
             string input = Console.ReadLine();
 
             switch (input)
@@ -50,7 +56,8 @@ class TaskManager
                 case "4": RemoveTask(); break;
                 case "5": DisplayAsCompletedTasks(); break;
                 case "6": DisplayAsDeletedTasks(); break;
-                case "7": Console.WriteLine("\nСпасибо за использование Task Manager!");
+                case "7": Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("\nСпасибо за использование Task Manager!");
                     Console.WriteLine(@"
 `````````_```````````_```````````````````````````` 
 ```__``_/`|_`__`___`|`|__``_`__```___`_```_`___```
@@ -60,7 +67,8 @@ class TaskManager
 ``` ````````````````````|_|`````````|___/`````````       
 ");
                     Thread.Sleep(3000); return;
-                case "02042008": Console.WriteLine("\nПривет от разработчика"); break;
+                case "02042008": Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("\nПривет от разработчика"); break;
                 default: Console.WriteLine("\nНекорректный ввод. Повторите попытку."); break;
             }
         }
@@ -75,22 +83,28 @@ class TaskManager
         Console.Write("Введите время выполнения задачи (15:30): ");
         DateTime.TryParseExact(Console.ReadLine(), "HH:mm", null, System.Globalization.DateTimeStyles.None, out var time);
         tasks.Add(new Task(title, date, time));
+        Console.ForegroundColor = ConsoleColor.Green;
         Console.WriteLine("\nЗадача успешно добавлена");
+        Console.ResetColor();
     }
 
     static void DisplayTasks()
     {
         if (tasks.Count == 0)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nСписок задач пуст.");
+            Console.ResetColor();
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nТекущие задачи:");
             for (int i = 0; i < tasks.Count; i++)
             {
                 Console.WriteLine($"\n{i + 1}. {tasks[i].Title} - {tasks[i].Date.ToString("dd.MM.yyyy")} {tasks[i].Time.ToString("HH:mm")}");
             }
+            Console.ResetColor();
         }
     }
 
@@ -98,7 +112,9 @@ class TaskManager
     {
         if (tasks.Count == 0)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nСписок задач пуст. Добавьте новую задачу.");
+            Console.ResetColor();
         }
         else
         {
@@ -108,11 +124,15 @@ class TaskManager
             {
                 tasksAsCompleted.Add(tasks[taskNumber - 1]);
                 tasks.RemoveAt(taskNumber - 1);
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\nЗадача успешно отмечена как выполненная.");
+                Console.ResetColor();
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("\nНекорректный номер задачи. Повторите попытку.");
+                Console.ResetColor();
             }
         }
     }
@@ -121,7 +141,9 @@ class TaskManager
     {
         if (tasks.Count == 0)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nСписок задач пуст. Добавьте новую задачу.");
+            Console.ResetColor();
         }
         else
         {
@@ -131,11 +153,15 @@ class TaskManager
             {
                 tasksAsDeleted.Add(tasks[taskNumber - 1]);
                 tasks.RemoveAt(taskNumber - 1);
+                Console.ForegroundColor = ConsoleColor.Green;
                 Console.WriteLine("\nЗадача успешно удалена.");
+                Console.ResetColor();
             }
             else
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("\nНекорректный номер задачи. Повторите попытку.");
+                Console.ResetColor();
             }
         }
     }
@@ -144,24 +170,29 @@ class TaskManager
     {
         if (tasksAsCompleted.Count == 0)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nСписок выполненных задач пуст");
+            Console.ResetColor();
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nВыполненные задачи:");
             for (int i = 0; i < tasksAsCompleted.Count; i++)
             {
                 Console.WriteLine($"\n{i + 1}. {tasksAsCompleted[i].Title} - {tasksAsCompleted[i].Date.ToString("dd.MM.yyyy")} {tasksAsCompleted[i].Time.ToString("HH:mm")}");
-
             }
             
+            Console.ResetColor();
+            
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("\nВыберите действие: \n1. Очистить список \n2. Выйти в главное меню\n");
 
             string input = Console.ReadLine();
 
             switch (input)
             {
-                case "1": tasksAsCompleted.Clear(); Console.WriteLine("\nВы очистили список выполненных задач"); break;
+                case "1": tasksAsCompleted.Clear(); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("\nВы очистили список выполненных задач"); Console.ResetColor(); break;
                 case "2": break;
             }
         }
@@ -173,23 +204,29 @@ class TaskManager
     {
         if (tasksAsDeleted.Count == 0)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("\nСписок удаленных задач пуст");
+            Console.ResetColor();
         }
         else
         {
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("\nУдаленные задачи");
             for (int i = 0; i < tasksAsDeleted.Count; i++)
             {
                 Console.WriteLine($"\n{i + 1}. {tasksAsDeleted[i].Title} - {tasksAsDeleted[i].Date.ToString("dd.MM.yyyy")} {tasksAsDeleted[i].Time.ToString("HH:mm")}");
             }
+            Console.ResetColor();
             
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("\nВыберите действие: \n1. Очистить список \n2. Выйти в главное меню\n");
-
+            Console.ResetColor();
+            
             string input = Console.ReadLine();
 
             switch (input)
             {
-                case "1": tasksAsDeleted.Clear(); Console.WriteLine("\nВы очистили список удаленных задач"); break;
+                case "1": tasksAsDeleted.Clear(); Console.ForegroundColor = ConsoleColor.Green; Console.WriteLine("\nВы очистили список удаленных задач"); Console.ResetColor(); break;
                 case "2": break;
             }
         }
